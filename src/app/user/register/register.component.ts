cf5f3dee48b6664493ebff93615784f9d3fcc24c
Rhett Harrison <reharri7@asu.edu>
2022-01-11 18:53:35 -0800

    Second commit

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 
 @Component({
@@ -11,7 +13,8 @@ import { AuthService } from 'src/app/services/auth.service';
 export class RegisterComponent {
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private emailTaken: EmailTaken
     ) {
 
   }
@@ -25,7 +28,7 @@ export class RegisterComponent {
   email = new FormControl('', [
     Validators.required,
     Validators.email
-  ]);
+  ], [this.emailTaken.validate]);
   age = new FormControl('',[
     Validators.required,
     Validators.min(18),
@@ -55,7 +58,7 @@ export class RegisterComponent {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber
-  });
+  }, [RegisterValidators.match('password', 'confirm_password')]);
 
   async register() {
     this.inSubmission = true;
