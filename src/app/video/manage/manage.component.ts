03895bd1f9cb58ce9c2098e2537b667ce87dfd1c
Rhett Harrison <reharri7@asu.edu>
2022-01-15 14:50:44 -0800

    Fifth commit

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Params, Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { ClipService } from '../../services/clip.service';
+import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-manage',
@@ -9,14 +13,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
+  clips: IClip[] = [];
+  activeClip: IClip | null = null;
+  sort$: BehaviorSubject<string>;
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService,
+    ) {
+      this.sort$ = new BehaviorSubject(this.videoOrder);
+
+    }
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : '1';
+      this.sort$.next(this.videoOrder);
+    });
+    this.clipService.getUserClips(this.sort$).subscribe(docs => {
+      this.clips = [];
+      docs.forEach(doc => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data()
+        });
+      });
     });
   }
 
@@ -30,4 +53,28 @@ export class ManageComponent implements OnInit {
     });
   }
 
+  openModal($event: Event, clip:IClip) {
+    $event.preventDefault();
+    this.activeClip = clip;
+    this.modal.toggleModal('editClip');
+  }
+
+  update($event : IClip) {
+    this.clips.forEach((element, index) => {
+      if(element.docID == $event.docID){
+        this.clips[index].title = $event.title;
+      }
+    });
+  }
+
+  deleteClip($event: Event, clip: IClip){
+    $event.preventDefault();
+
+    this.clipService.deleteClip(clip);
+    this.clips.forEach( (element, index) => {
+      if(element.docID == clip.docID) {
+        this.clips.splice(index, 1);
+      }
+    });
+  }
 }
--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -5,17 +5,22 @@ import { VideoRoutingModule } from './video-routing.module';
 import { UploadComponent } from './upload/upload.component';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    UploadComponent
+    UploadComponent,
+    EditComponent
+
   ],
   imports: [
     CommonModule,
     VideoRoutingModule,
     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+  ],
+  exports:[
+    EditComponent
   ]
 })
 export class VideoModule { }
